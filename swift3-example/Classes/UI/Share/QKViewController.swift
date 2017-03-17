83f221639aa7328b618b4c3bd3322a79d311c8e4
QuangD8 <quangpham@d8ii.com>
2017-03-17 13:55:05 +0700

    Init project

---
+++ swift3-example/Classes/UI/Share/QKViewController.swift
@@ -0,0 +1,31 @@
+//
+//  QKViewController.swift
+//  swift3-example
+//
+//  Created by Quang Co on 3/17/17.
+//  Copyright © 2017 quanky. All rights reserved.
+//
+// This classes should use to handle general setup, for example: Status bar style.
+
+
+import UIKit
+
+class QKViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+
+}
